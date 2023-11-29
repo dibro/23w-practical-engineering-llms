c5094b51a5d3902575891a5d7f1418e7b07d7567
Khanh Ho <hpk0304@gmail.com>
2023-11-29 21:38:16 +0100

    Document embeddings and vector store

--- experiment.py
+++ experiment.py
@@ -2,10 +2,9 @@ import os
 import re
 import getpass
 import langchain
-from langchain.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import CacheBackedEmbeddings
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.storage import LocalFileStore
 from typing import List, Union
@@ -17,6 +16,7 @@ from langchain.chat_models import ChatOpenAI
 
 os.environ["OPENAI_API_KEY"] = getpass.getpass("Your OpenAI API Key:")
 
+
 def split_text(document: List) -> List[langchain.schema.document.Document]:
     """Two type of split: 
     - CharacterTextSplitter: split based on characters passed in, \n
@@ -24,6 +24,7 @@ def split_text(document: List) -> List[langchain.schema.document.Document]:
     - RecursiveTextSplitter: split based on semantical units (e.g sentences) \n
     chunk size is numberd of characters or tokens -> We use this 
     """
+    print("Splitting process...")
     text_split = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=50,
@@ -32,4 +33,39 @@ def split_text(document: List) -> List[langchain.schema.document.Document]:
     chunks = text_split.transform_documents(document)
     print('Splitting done')
     return chunks
+
+def doc_embedding(chunk: List, emb_type) -> langchain.embeddings.cache.CacheBackedEmbeddings:
+    """
+    Construct an Embedder (get embeddings from chunks) with
+    - CacheBackedEmbeddings: local cache
+    - Some types of embeddings (emb_type): OpenAI, HuggingFace, etc
+    """
+    print("Constructing Embedder...")
+    store = LocalFileStore("./cache/")
+    # Choose embedding type
+    if emb_type = 'openai':
+        emb_model = OpenAIEmbeddings()
+    if emb_type = 'huggingface':
+        emb_model = HuggingFaceEmbeddings()
+    else:
+        raise ValueError(f'Unsupported embedding type: {emb_type}')
+    
+    embedder = CacheBackedEmbeddings(
+        emb_model,
+        store,
+        namespace=emb_model.model
+    )
+    print('Embedder is ready!')
+    return embedder
+
+
+def vector_store(chunks: List[langchain.schema.document.Document],
+                 embedder: langchain.embedding.cache.CacheBackedEmbeddings) -> langchain.vectorstores.faiss.FAISS:
+    """
+    Using embedder to transform chunks into vectors,
+    then using FAISS to store them
+    """
+    print('Creating vector store...')
+    vector_store = FAISS.from_documents(chunks, embedder)
+    return vector_store
 
